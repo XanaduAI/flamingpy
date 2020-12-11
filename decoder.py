6bf29dd7e876232007e7dca0ba8b33109d6f421d
ilan-tz <57886357+ilan-tz@users.noreply.github.com>
2020-12-10 20:08:30 -0500

    Update decoder.py with functions for obtaining a decoding graph from the RHG lattice
    Add functions RHG_syndrome_coords (ported from graphstates.py) for obtaining coordinates associated with six-body X stabilizer elements; RHG_stabilizers for making a list of subgraphs associated to the stabilizer elements; parity for computing the parity of a stabilizer element; assign_weights to do a blueprint-type weight assignment to qubits of the RHG lattice; and decoding_graph for using the above functions to generate a decoding graph that will be used to produce a matching graph.

--- decoder.py
+++ decoder.py
@@ -12,22 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 '''The decoder module'''
-# import itertools as it
-# import networkx as nx
-# import numpy as np
-# from numpy.random import (multivariate_normal as mvn, default_rng as rng)
-# import matplotlib.pyplot as plt
-# import matplotlib.lines as mlines
-# from scipy.special import erf
+
+import numpy as np
+import networkx as nx
+import itertools as it
 
 from graphstates import EGraph, CVGraph, RHG_graph
 
 
-def decoding_graph(G):
-    return G
+def RHG_syndrome_coords(dims, code='primal'):
+    """Return a list of lists of coordinates associated to six-body
+    X stabilizer elements of the RHG lattice."""
+
+    nx, ny, nz = dims
+    combs = it.product(range(nx), range(ny), range(nz))
+    if code == 'primal':
+        all_syndrome6 = [[(2*i+1, 2*j+1, 2*k),
+                          (2*i+1, 2*j, 2*k+1),
+                          (2*i, 2*j+1, 2*k+1),
+                          (2*i+1, 2*j+1, 2*k+2),
+                          (2*i+1, 2*j+2, 2*k+1),
+                          (2*i+2, 2*j+1, 2*k+1)
+                          ] for (i, j, k) in combs]
+    return all_syndrome6
+
+
+def RHG_stabilizers(G, code='primal'):
+    """Return a list of subgraphs induced by the qubits with cordinates
+    from RHG_syndrome_coords."""
+
+    syn_list = RHG_syndrome_coords(G.graph.graph['dims'], code)
+    cube_list = []
+    for l in syn_list:
+        cube_graph = G.graph.subgraph(l)
+        cube_list.append(CVGraph(cube_graph))
+    return cube_list
+
+
+def parity(cube):
+    """Obtain the parity of the bit values following a measurement of
+    the a six-body X stabilizer element represented in cube."""
+
+    bit_vals = [cube.graph.nodes[node]['bit_val'] for node in cube.graph]
+    return np.sum(bit_vals) % 2
 
 
 def assign_weights(CVG):
+    """Assign weights to qubits in a hybrid p-squeezed/GKP CV graph 
+    state per the heuristic procedure in the blueprint."""
+
     G = CVG.graph
     for node in G:
         neighbors = G.subgraph(G[node]).nodes
@@ -40,22 +73,39 @@ def assign_weights(CVG):
                   'use eval_Z_probs() first.')
             return
         weight_dict = {0: err_prob, 1: err_prob, 2: 1/4, 3: 1/3, 4: 2/5}
-        G.nodes[node]['weight'] = weight_dict[p_count]
+        G.nodes[node]['weight'] = -np.log(weight_dict[p_count])
     return
 
-if __name__ == '__main__':
-    RHG = RHG_graph(1)
-    # RHG.draw(label=1)
 
-    dim = 2
+def decoding_graph(G, code='primal'):
+    """Create a decoding graph from the RHG lattice G. Note that one 
+    must first compute the phase error probabilities, conduct a 
+    homodyne measurement, and translate the outcomes on G."""
+
+    G_dec = nx.Graph()
+    stabes = RHG_stabilizers(G, code)
+    for stabe in stabes:
+        if parity(stabe) == 1:
+            G_dec.add_node(stabe)
+    for (stabe1, stabe2) in it.combinations(G_dec, 2):
+        common_vertex = set(stabe1.graph.nodes).intersection(stabe2.graph.nodes)
+        if common_vertex:
+            weight = G.graph.nodes[common_vertex.pop()]['weight']
+            G_dec.add_edge(stabe1, stabe2, weight=weight)
+    return G_dec
+
+
+if __name__ == '__main__':
+    RHG = RHG_graph(2)
+
+    swap_prob = 0.2
     delta = 0.1
 
-    N = RHG.number_of_nodes()
-
-    p = 0.3
-    G = CVGraph(RHG, swap_prob=0.5, delta=delta)
+    G = CVGraph(RHG, swap_prob=swap_prob, delta=delta)
     G.eval_Z_probs()
-    # G.measure_p()
-    # for label in {'v', 'e', 'h', 'b'}:
-    #     G.sketch(label)
+    G.measure_p()
+    G.translate_outcomes()
     assign_weights(G)
+
+    G.sketch('bit_val')
+    G_dec = decoding_graph(G)
