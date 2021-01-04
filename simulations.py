07c7158414c6a174972faad71898c3bbc173ea4a
ashlesha-p <ashlesha@email.arizona.edu>
2021-01-04 11:05:57 -0700

    Update simulations.py

--- simulations.py
+++ simulations.py
@@ -13,3 +13,37 @@
 # limitations under the License.
 """Module for Monte Carlo simulations for estimating FT thresholds."""
 
+#%%
+import numpy as np
+from decoder import *
+#%%
+
+def monte_carlo(iterations,L,swap_prob,delta):
+    corrected = 0
+    for i in range(iterations):
+        RHG_lattice = RHG.RHG_graph(L, pol=1)
+        G = CVGraph(RHG_lattice, swap_prob=swap_prob, delta=delta)
+        G.eval_Z_probs()
+        G.measure_p()
+        G.translate_outcomes()
+        assign_weights(G, method='blueprint')
+        
+        dw = {'show_nodes': False, 'label_nodes': '', 'label_cubes': True,
+              'label_boundary': False, 'legend':False}
+        G_dec = decoding_graph(G, bc='', drawing_opts=dw, draw=False)
+        G_match = matching_graph(G_dec, bc='', draw=False)
+        matching = MWPM(G_match, G_dec, draw=False)
+        
+        #if correction_consitions_satisfied:
+            #corrected = corrected+1
+        
+        
+    return corrected/iterations
+
+#%%
+iterations = 10000
+L = 2
+swap_prob = 0.2
+delta = 0.01
+
+monte_carlo(iterations,L,swap_prob,delta)
