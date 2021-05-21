ce2a1ab22f17cb57d664a3410efec875ebef086f
ilan-tz <57886357+ilan-tz@users.noreply.github.com>
2021-05-21 16:16:02 -0400

    Add ability to do passive architecture simulations to simulations.py

--- simulations.py
+++ simulations.py
@@ -18,10 +18,11 @@ import sys
 from datetime import datetime
 from decoder import correct
 from graphstates import CVGraph
-from RHG import RHGCode
+from RHG import RHG_graph, RHGCode
+from passive_construct import BS_network, reduce_macro_and_simulate
 
 
-def ec_monte_carlo(code, trials, delta, p_swap):
+def ec_monte_carlo(code, trials, delta, p_swap, passive_objects):
     """Run Monte Carlo simulations of error-correction on code code.
 
     Given a code object code, a noise parameter delta, and a
@@ -39,26 +40,35 @@ def ec_monte_carlo(code, trials, delta, p_swap):
     Returns:
         float: the failure probability.
     """
-    # TODO: Noise model input.
-    code_lattice = code.graph
-    # Noise model
-    CVRHG = CVGraph(code_lattice, p_swap=p_swap)
-    cv_noise = {"noise": "grn", "delta": delta, "sampling_order": "initial"}
-    # Decoding options
-    decoder = {"inner": "basic", "outer": "MWPM"}
-    weight_options = {
-        "method": "blueprint",
-        "integer": False,
-        "multiplier": 1,
-        "delta": delta,
-    }
+    if passive_objects:
+        RHG_macro, RHG_reduced, CVRHG_reduced, bs_network = passive_objects
+        decoder = {"outer": "MWPM"}
+        weight_options = {"method": "blueprint", "prob_precomputed": True}
+    else:
+        # TODO: Noise model input.
+        code_lattice = code.graph
+        # Noise model
+        CVRHG = CVGraph(code_lattice, p_swap=p_swap)
+        cv_noise = {"noise": "grn", "delta": delta, "sampling_order": "initial"}
+        # Decoding options
+        decoder = {"inner": "basic", "outer": "MWPM"}
+        weight_options = {
+            "method": "blueprint",
+            "integer": False,
+            "multiplier": 1,
+            "delta": delta,
+        }
 
     successes = 0
     for _ in range(trials):
-        # Apply noise
-        CVRHG.apply_noise(cv_noise)
-        # Measure syndrome
-        CVRHG.measure_hom("p", code.syndrome_inds)
+        if passive_objects:
+            reduce_macro_and_simulate(*passive_objects, p_swap, delta)
+        else:
+            # Apply noise
+            CVRHG.apply_noise(cv_noise)
+            # Measure syndrome
+            CVRHG.measure_hom("p", code.syndrome_inds)
+
         result = correct(code=code, decoder=decoder, weight_options=weight_options)
         successes += result
         errors = trials - successes
@@ -94,9 +104,30 @@ if __name__ == "__main__":
         distance, delta, p_swap, trials, passive = 2, 0.01, 0.5, 100, False
 
     # The Monte Carlo simulations
-    boundaries = "finite"
-    RHG_code = RHGCode(distance, boundaries=boundaries, polarity=True)
-    errors = ec_monte_carlo(RHG_code, trials, delta, p_swap)
+    if passive:
+        # The lattice with macronodes.
+        RHG_macro = RHG_graph(
+            distance, boundaries="periodic", macronodes=True, polarity=False
+        )
+        RHG_macro.index_generator()
+        RHG_macro.adj_generator(sparse=True)
+        # The reduced lattice.
+        RHG_code = RHGCode(distance, boundaries="periodic")
+        RHG_reduced = RHG_code.graph
+        RHG_reduced.index_generator()
+        # The empty CV state, uninitiated with any error model.
+        CVRHG_reduced = CVGraph(RHG_reduced)
+
+        # Define the 4X4 beamsplitter network for a given macronode.
+        # star at index 0, planets at indices 1-3.
+        bs_network = BS_network(4)
+        passive_objects = [RHG_macro, RHG_reduced, CVRHG_reduced, bs_network]
+    else:
+        boundaries = "finite"
+        RHG_code = RHGCode(distance, boundaries=boundaries, polarity=True)
+        passive_objects = None
+
+    errors = ec_monte_carlo(RHG_code, trials, delta, p_swap, passive_objects)
 
     # Store results in the data directory in the file results.csv.
     file_name = "./data/results.csv"
