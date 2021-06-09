ec0f6619268ab119926f451e27726b6dd67f77bc
ilan-tz <57886357+ilan-tz@users.noreply.github.com>
2021-06-09 14:38:57 -0400

    Tidy up, comment, update, add tests to test_decoder
    Mainly improved decoding graph tests and included new tests of the correction check.

--- test_decoder.py
+++ test_decoder.py
@@ -14,21 +14,29 @@
 """"Unit tests for decoder funcions in decoder.py."""
 
 import pytest
+import sys
+import io
+from ast import literal_eval
+import numpy as np
 from graphstates import CVGraph
-from decoder import (assign_weights,
-                     CV_decoder,
-                     decoding_graph,
-                     matching_graph,
-                     MWPM,
-                     recovery,
-                     check_correction,
-                     correct)
+from decoder import (
+    assign_weights,
+    CV_decoder,
+    decoding_graph,
+    matching_graph,
+    MWPM,
+    recovery,
+    check_correction,
+    correct,
+)
 from RHG import RHGCode, RHGCube
 import networkx as nx
 import itertools as it
 
-code_params = it.product([2, 4], ['finite', 'periodic'], [1, 0.1, 0.01], [0, 0.5, 1])
+code_params = it.product([2, 3, 4], ["finite", "periodic"], [1, 0.1, 0.01], [0, 0.5, 1])
 
+
+# An RHGCode object as well as an encoded CVGraph for use in this module.
 @pytest.fixture(scope="module", params=code_params)
 def enc_state(request):
     distance, boundaries, delta, p_swap = request.param
@@ -44,6 +52,8 @@ def enc_state(request):
     CVRHG.measure_hom("p", DVRHG.syndrome_inds)
     return DVRHG, CVRHG
 
+
+# The decoding graphs for use in this module.
 @pytest.fixture(scope="module")
 def dec_graphs(enc_state):
     G_dec = decoding_graph(enc_state[0])
@@ -51,13 +61,15 @@ def dec_graphs(enc_state):
     matching = MWPM(G_match, G_dec)
     return G_dec, G_match, matching
 
+
 class TestAssignWeights:
     "Test the weight assignment in decoder.py."
 
     def test_unit_weights(self, enc_state):
         assign_weights(enc_state[0], method="unit")
+        # Check that all weights are 1 with the "unit" weight option.
         for point in enc_state[0].syndrome_coords:
-            assert enc_state[0].graph.nodes[point]['weight'] == 1
+            assert enc_state[0].graph.nodes[point]["weight"] == 1
 
     def test_blueprint_weights(self, enc_state):
         weight_options = {
@@ -65,45 +77,78 @@ class TestAssignWeights:
             "integer": True,
             "multiplier": 100,
             "delta": enc_state[1]._delta,
-            }
+        }
         assign_weights(enc_state[0], **weight_options)
         for point in enc_state[0].syndrome_coords:
-            weight = enc_state[0].graph.nodes[point]['weight']
+            weight = enc_state[0].graph.nodes[point]["weight"]
+            # Check that all the weights are positive integers given
+            # the weight options above.
             assert isinstance(weight, int)
             assert weight >= 0
 
-class TestDecoder:
 
+class TestDecoder:
     def test_CV_decoder(self, enc_state):
         CV_decoder(enc_state[0])
-        assert enc_state[1].bit_values
+        bits = enc_state[1].bit_values()
+        # Check that bit values have been computed, and that they are
+        # either 0 or 1.
+        assert bits
+        for point in enc_state[0].syndrome_coords:
+            bit_val_attribute = enc_state[0].graph.nodes[point]["bit_val"]
+            assert bit_val_attribute in (0, 1)
+            index = enc_state[0].graph.to_indices[point]
+            # Check that there is not index mismatch between bit list
+            # from CVGraph.bit_values() and bits stored inside the graph
+            # attributes.
+            assert bits[index] == bit_val_attribute
+        # Check that all the stabilizer cubes point to nodes with bit
+        # values
+        for cube in enc_state[0].stabilizers:
+            for point in cube.egraph:
+                assert cube.egraph.nodes[point].get("bit_val") is not None
 
-    def test_decoding_graph(self, dec_graphs):
+    def test_decoding_graph(self, dec_graphs, enc_state):
         G_dec = dec_graphs[0]
-        assert G_dec.graph['title'] == 'Decoding Graph'
-        assert "odd_cubes" in G_dec.graph
-        boundary_points = G_dec.graph["boundary_points"]
-        for node in G_dec.nodes - boundary_points - {'low', 'high'}:
-            assert isinstance(G_dec.nodes[node]['stabilizer'], RHGCube)
+        assert G_dec.graph["title"] == "Decoding Graph"
+        odd_cubes = G_dec.graph["odd_cubes"]
+        num_stabes = len(enc_state[0].stabilizers)
+        even_cubes = set(range(num_stabes)) - set(odd_cubes)
+        print(odd_cubes, even_cubes)
+        print(G_dec.nodes)
+        boundary_points = set(G_dec.graph["boundary_points"])
+        # Check odd and even cubes appropriately indexed.
+        for cube_index in odd_cubes:
+            cube = G_dec.nodes[cube_index]["stabilizer"]
+            assert cube.parity == 1
+        for cube_index in even_cubes - boundary_points - {"low", "high"}:
+            cube = G_dec.nodes[cube_index]["stabilizer"]
+            assert cube.parity == 0
+        # Check that edges contain the coordinates of the common vertex
+        # between neighbouring stabilizers.
         for edge in G_dec.edges:
-            assert 'weight' in G_dec.edges[edge]
-            if ('high' not in edge) and ('low' not in edge):
-                assert 'common_vertex' in G_dec.edges[edge]
+            assert "weight" in G_dec.edges[edge]
+            if ("high" not in edge) and ("low" not in edge):
+                assert "common_vertex" in G_dec.edges[edge]
 
     def test_matching_graph(self, dec_graphs):
         G_match = dec_graphs[1]
         assert G_match.graph["title"] == "Matching Graph"
-        assert "virtual_points" in G_match.graph
         virtual_points = G_match.graph["virtual_points"]
         remaining_points = G_match.nodes - virtual_points
         n_virt = len(virtual_points)
         n_stabes = len(remaining_points)
         virtual_subgraph = G_match.subgraph(virtual_points)
         stabilizer_subgraph = G_match.subgraph(remaining_points)
+        # Check that the graph formed between the virtual boundary excitations,
+        # as well as the graph formed between the nodes corresponding to
+        # stabilizers, are both complete graphs.
         assert nx.is_isomorphic(virtual_subgraph, nx.complete_graph(n_virt))
         assert nx.is_isomorphic(stabilizer_subgraph, nx.complete_graph(n_stabes))
+        # Check the the edges in the graph formed between the boundary
+        # excitations all have weight 0.
         for edge in virtual_subgraph.edges:
-            assert virtual_subgraph.edges[edge]['weight'] == 0
+            assert virtual_subgraph.edges[edge]["weight"] == 0
 
     def test_MWPM(self, dec_graphs):
         # Check that the matching is perfect (the set of all the nodes
@@ -114,12 +159,46 @@ class TestDecoder:
 
 
 class TestRecovery:
-
     def test_recovery(self, enc_state, dec_graphs):
         recovery(enc_state[0], dec_graphs[1], dec_graphs[0], dec_graphs[2])
         G_dec_new = decoding_graph(enc_state[0], draw=False)
         odd_cubes = G_dec_new.graph["odd_cubes"]
+        # Check that there remain no unsatisfied stabilizers after the
+        # recovery operation.
         assert not odd_cubes
 
-    def test_corection_check(self):
+    def test_corection_check(self, enc_state):
+        # Capture sanity check printout
+        old_stdout = sys.stdout
+        new_stdout = io.StringIO()
+        sys.stdout = new_stdout
+
+        result = check_correction(enc_state[0], sanity_check=True)
+
+        output = new_stdout.getvalue()
+        sys.stdout = old_stdout
+
+        surface_dict = literal_eval(output)
+        boundaries = enc_state[0].boundaries
+        if boundaries == ["periodic"] * 3:
+            planes = ["x", "y", "z"]
+        if tuple(boundaries) in set(it.permutations(["primal", "dual", "dual"])):
+            where_primal = np.where(np.array(boundaries) == "primal")[0][0]
+            planes = [["x", "y", "z"][where_primal]]
+        failure_events = 0
+        for plane in planes:
+            plane_parities = np.array(surface_dict[plane])
+            # Check that parity along a plane is conserved.
+            assert np.all(plane_parities) or np.all(plane_parities ^ 1)
+            if np.all(plane_parities ^ 1):
+                failure_events += 1
+        print(planes)
+        print(surface_dict)
+        print(failure_events, result)
+        # Check that error correction fails in case of at least one
+        # plane with odd parity.
+        if failure_events:
+            assert not result
+
+    def test_correct(self):
         pass
