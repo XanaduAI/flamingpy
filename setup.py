2eb24df33946c583ed40649bf8d7885b91574d34
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-01-07 12:56:32 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,14 @@
 # limitations under the License.
 
 import os
+import re
 import sys
+import platform
 import subprocess
 
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 # The following two classes are adaptations of the Python example for pybind11:
 # https://github.com/pybind/python_example/blob/master/setup.py
@@ -29,28 +32,45 @@ class CMakeExtension(Extension):
 
 
 class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
     def build_extension(self, ext):
-        path = self.get_ext_fullpath(ext.name)
-        extdir = os.path.abspath(os.path.dirname(path))
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        # Required for auto-detection of auxiliary "native" libs.
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
 
-        os.makedirs(self.build_temp, exist_ok=True)
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
 
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        cmake_args = [
-            "-DCMAKE_BUILD_TYPE=Release",
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-        ]
-
-        cmake_cmd = ["cmake", ext.sourcedir] + cmake_args
-        build_cmd = ["cmake", "--build", "."]
-
-        subprocess.check_call(cmake_cmd, cwd=self.build_temp)
-        subprocess.check_call(build_cmd, cwd=self.build_temp)
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
     name="ft-stack",
