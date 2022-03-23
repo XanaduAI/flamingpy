c0e2af39b80dab7de320b369be8a1a26460a7279
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-03-23 16:54:04 +1100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -23,6 +23,7 @@ sys.path.append(os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(".")), "doc"))
 
+autodoc_mock_imports = ['numpy']
 
 # -- Project information -----------------------------------------------------
 
