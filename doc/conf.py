fd212c572423a8acacdb4e0af6023f8d9faa6958
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-03-23 18:59:07 +1100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -63,10 +63,10 @@ MOCK_MODULES = [
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
     
-automodapi_toctreedirnm = "source/api"
-automodsumm_inherited_members = True
-autosummary_generate = True
-autosummary_imported_members = False    
+#automodapi_toctreedirnm = "source/api"
+#automodsumm_inherited_members = True
+#autosummary_generate = True
+#autosummary_imported_members = False    
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
