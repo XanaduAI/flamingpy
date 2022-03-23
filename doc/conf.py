c21cb4eeb722794efd231f7db4e8b4525928d69b
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-03-23 16:59:52 +1100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -12,9 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-import os
-import sys
-import subprocess
+import os, sys, subprocess, re
 from unittest import mock
 
 sys.path.insert(0, os.path.abspath("_ext"))
