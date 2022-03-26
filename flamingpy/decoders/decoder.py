ae8c684d9399ba5c29d11c24640cff5309a95a14
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-03-27 10:03:12 +1100

    Update decoder.py

--- flamingpy/decoders/decoder.py
+++ flamingpy/decoders/decoder.py
@@ -276,7 +276,7 @@ def correct(
             'method': 'unit' or 'blueprint'
             'integer': True (for rounding) or False (for not)
             'multiplier': integer denoting multiplicative factor
-                before rounding Unit weights by default.
+            before rounding Unit weights by default.
         sanity_check (bool, optional): if True, check that the recovery
             operation succeeded and verify that parity is conserved
             among all correlation surfaces
