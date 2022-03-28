1db0e8efce9d2d1b486cb016885976aa67b6bbca
nariman87 <25132802+nariman87@users.noreply.github.com>
2022-03-28 15:10:36 +1100

    black formatting ...

--- tests/test_benchmarks_examples.py
+++ tests/test_benchmarks_examples.py
@@ -19,37 +19,46 @@ def test_decoder_example():
     """Simple tests for decoding module in flamingpy.examples"""
     from flamingpy.examples import decoding as dc_examples
 
+
 def test_decoding_benchmark():
     """Simple tests for decoding module in flamingpy.benchmarks"""
     from flamingpy.benchmarks import decoding as dc_benchmarks
+
 
 def test_gkp_example():
     """Simple tests for gkp module in flamingpy.examples"""
     from flamingpy.examples import gkp
 
+
 def test_graphstates_example():
     """Simple tests for graphstates module in flamingpy.examples"""
     from flamingpy.examples import graphstates
+
 
 def test_macro_reduce_example():
     """Simple tests for macro_reduce module in flamingpy.examples"""
     from flamingpy.examples import macro_reduce
 
+
 def test_surface_code_example():
     """Simple tests for surface_code module in flamingpy.examples"""
     from flamingpy.examples import surface_code
+
 
 def test_lemon_benchmark():
     """Simple tests for lemon module in flamingpy.benchmarks"""
     from flamingpy.benchmarks import lemon
 
+
 def test_matching_benchmark():
     """Simple tests for matching module in flamingpy.benchmarks"""
     from flamingpy.benchmarks import matching
 
+
 def test_shortest_path_benchmark():
     """Simple tests for shortest_path module in flamingpy.benchmarks"""
     from flamingpy.benchmarks import shortest_path
+
 
 def test_simulations_benchmark():
     """Simple tests for simulations module in flamingpy.benchmarks"""
