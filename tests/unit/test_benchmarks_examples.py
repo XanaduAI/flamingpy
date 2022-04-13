193266d16b5a9288e067dc3124da5c560bbff888
Narimanium <25132802+nariman87@users.noreply.github.com>
2022-04-13 20:12:59 +1000

    Updating test_benchmarks_examples.py and test_graphstates.py

--- tests/unit/test_benchmarks_examples.py
+++ tests/unit/test_benchmarks_examples.py
@@ -14,6 +14,8 @@
 """Check that we can run all the example and benchmark files without showing
 the plots."""
 
+# pylint: disable=import-outside-toplevel
+
 
 def test_decoder_example():
     """Simple test for the decoding module in flamingpy.examples."""
