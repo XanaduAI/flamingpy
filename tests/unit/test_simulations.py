31c2fb9f36bafa9fb6f42b81049c341217671ba2
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2022-05-04 14:18:43 -0500

    Improve test coverage (#14)
    * test [test_graphstates] - test_SCZ_apply
    
    * refactor + test [simulations]
    
    - refactor `simulations.py` so that setup logic is on its own function (instead of the `__main__` scope) and can be tested
    -  test the generated output has the expected header and number of columns
    
    * format - black
    
    * linting- codefactor
    
    * coverage [utils/viz] - no coverage for `plt.plot` lines
    
    * test [iid_noise] - exception raising
    
    * refactor + test [examples/surface_code]
    
    * test [examples/surface_code] - remove coverage from `" __main__"`
    
    * test [examples/graphstates] - remove coverage from `plt.plot` lines
    
    * refactor + test [examples/decoding]
    
    * test [examples/decoding] - remove coverage from `" __main__"`
    
    * test [simulations] - append result to created file
    
    * test [__init__] - remove coverage from ImportError
    
    * test [__init__] - version
    
    * refactor+test [benchmarks+simulations] - check execution output to file
    
    * test [benchmarks] - remove coverage from __main__
    
    * test [decoders/mwpm/lemon] - ignore coverage on exception raising
    
    * format - pylint
    
    * format - add docstrings
    
    * workflows [tests] - `cov-fail-under=95`
    
    * fix [examples/decoding] - `draw_matching_on_syndrome_plot` args
    
    * fix [coverage] - ignore `# pragma: no cover` lines
    
    * fix [test_init] - remove duplicated test
    
    * coverage [coveragerc] - move unnecessary `# pragma: no cover` to `exclude_lines`
    
    * Apply suggestions from code review
    
    Co-authored-by: ilan-tz <57886357+ilan-tz@users.noreply.github.com>
    
    * refactor [test_simulations] - merge tests
    
    * format - black + docs
    
    * refactor [test_init] -  test_version
    
    * fix [test_simulations] - output file
    
    * fix [simulations] - execution from main
    
    * refactor [examples] - decoding: draw intermediate results
    
    * refactor [test_SCZ_apply]
    
    * apply suggestions from code review
    
    * add CHANGELOG entry
    
    * Update _version.py
    
    * Update _version.py
    
    Co-authored-by: ilan-tz <57886357+ilan-tz@users.noreply.github.com>

--- tests/unit/test_simulations.py
+++ tests/unit/test_simulations.py
@@ -13,16 +13,17 @@
 # limitations under the License.
 """Unit tests for Monte Carlo simulations for estimating FT thresholds."""
 
-# pylint: disable=no-self-use
+# pylint: disable=no-self-use,protected-access,too-few-public-methods
 
 import itertools as it
+import re
 import pytest
 
 from flamingpy.codes import alternating_polarity, SurfaceCode
 from flamingpy.cv.ops import CVLayer
 from flamingpy.cv.macro_reduce import BS_network
-from flamingpy.simulations import ec_monte_carlo
-
+from flamingpy.simulations import ec_monte_carlo, run_ec_simulation
+from flamingpy.benchmarks.simulations import run_sims_benchmark
 
 code_params = it.product([2, 3, 4], ["primal", "dual"], ["open", "periodic"])
 
@@ -62,7 +63,7 @@ class TestPassive:
         delta = 0.001
         trials = 10
 
-        pad_bool = False if code.bound_str == "periodic" else True
+        pad_bool = code.bound_str != "periodic"
         # The lattice with macronodes.
         RHG_macro = code.graph.macronize(pad_boundary=pad_bool)
         RHG_macro.index_generator()
@@ -78,3 +79,45 @@ class TestPassive:
         errors_py = ec_monte_carlo(code, trials, delta, p_swap, passive_objects=passive_objects)
         # Check that there are no errors in all-GKP high-squeezing limit.
         assert errors_py == 0
+
+
+@pytest.mark.parametrize("passive", [True, False])
+@pytest.mark.parametrize("empty_file", [True, False])
+@pytest.mark.parametrize("sim", [run_ec_simulation, run_sims_benchmark])
+def test_simulations_output_file(tmpdir, passive, empty_file, sim):
+    """Check the content of the simulation benchmark output file."""
+
+    expected_header = "distance,ec,boundaries,delta,p_swap,errors_py,trials,current_time"
+    dummy_content = "2,primal,open,0.04,0.5,2,10,12:34:56"
+    if "benchmark" in sim.__name__:
+        expected_header += ",cpp_to_py_speedup"
+        dummy_content += ",1"
+
+    f = tmpdir.join("sims_results.csv")
+    if not empty_file:
+        f.write_text(
+            f"{expected_header}\n" + "\n",
+            encoding="UTF-8",
+        )
+
+    # simulation params
+    params = {
+        "distance": 2,
+        "ec": "primal",
+        "boundaries": "open",
+        "delta": 0.04,
+        "p_swap": 0.5,
+        "passive": passive,
+        "trials": 10,
+    }
+    sim(**params, fname=f)
+
+    file_lines = f.readlines()
+    # file is created with header and result lines
+    assert len(file_lines) > 0
+
+    # contains the expected header
+    assert file_lines[0] == expected_header + "\n"
+
+    # contents has the expected number of columns
+    assert len(re.split(",", file_lines[-1])) == len(re.split(",", expected_header))
