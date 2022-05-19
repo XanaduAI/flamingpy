ba9fa92eb47362adc494e8a1de6c32d83c6e49da
ilan-tz <57886357+ilan-tz@users.noreply.github.com>
2022-05-19 15:37:12 -0400

    Rename and split up reduce_macro_and_simulate
    - Function now called reduce_macronode_graph
    - Function used to be massive, but now split up into 8 small functions

--- flamingpy/cv/macro_reduce.py
+++ flamingpy/cv/macro_reduce.py
@@ -30,7 +30,7 @@ def invert_permutation(p):
     return p_inverted
 
 
-def BS_network(n):
+def symp_splitter(n):
     """Return the symlectic matrix of the beamsplitter network.
 
     Return the symplectic matrix of the beamsplitters connecting four
@@ -60,55 +60,46 @@ def BS_network(n):
     return bs_network
 
 
-def reduce_macro_and_simulate(RHG_macro, RHG_reduced, CVRHG_reduced, bs_network, swap_prob, delta):
-    """Reduce the macronode RHG lattice to the canonical lattice.
-
-    Take the macronode lattice EGraph, RHG_macro, and generate a
-    macronode CV lattice with swap-out probaiblity swap_prob and delta
-    value delta. Then, label micronodes as planets and stars, conduct
-    homodyne measurements, process these measurements, and compute
-    conditional phase error probabilities. Generate an canonical RHG
-    lattice with effective measurement outcomes, phase error
-    probabilities, and state types ('p' or 'GKP') stored as node
-    attributes.
-    """
-    to_points = RHG_macro.to_points
-    N = len(RHG_macro)
-
-    # The hybridized CVRHG macronode lattice.
-    CVRHG = CVLayer(RHG_macro, p_swap=swap_prob)
-    # Noise-model
-    perfect_points = RHG_macro.graph.get("perfect_points")
+def _apply_initial_noise(macro_graph, noise_layer, noise_model):
+    """Apply initial noise to RHG_macro."""
+    to_points = macro_graph.to_points
+    N = len(macro_graph)
+    # Apply a CV noise layer to the macronode lattice.
+    noisy_macro_state = noise_layer(macro_graph, p_swap=swap_prob)
+    # Identify perfect nodes, if they exist
+    perfect_points = macro_graph.graph.get("perfect_points")
     if perfect_points:
-        perfect_inds = [RHG_macro.to_indices[point] for point in perfect_points]
+        perfect_inds = [macro_graph.to_indices[point] for point in perfect_points]
     else:
         perfect_inds = None
-    noise_model = {
-        "noise": "grn",
-        "delta": delta,
-        "sampling_order": "two-step",
-        "perfect_inds": perfect_inds,
-    }
-    CVRHG.apply_noise(noise_model)
+    noise_model["perfect_inds"] = perfect_inds
+    apply_noise(noise_model)
+    return noisy_macro_state
+
+
+def _permute_indices(code, macro_graph):
+    """Obtain permuted indices and set type of reduced node."""
     # A list of permuted indices where each block of four
     # corresponds to [star, planet, planet, planet].
+    reduced_graph = code.graph
+    reduced_graph._states = []
     permuted_inds = np.empty(N, dtype=np.int32)
     for i in range(0, N - 3, 4):
         # Indices of GKP micronodes in macronode i.
         gkps = []
         for j in range(4):
             micronode = to_points[i + j]
-            if CVRHG.egraph.nodes[micronode]["state"] == "GKP":
+            if macro_graph.nodes[micronode]["state"] == "GKP":
                 gkps.append(j)
         centre_point = tuple(round(i) for i in micronode)
         if gkps:
             star_ind, reduced_state = i + gkps[0], "GKP"
         else:
             star_ind, reduced_state = i, "p"
-            CVRHG_reduced._states["p"] += [RHG_reduced.to_indices[centre_point]]
+            # RHG_reduced._states += [RHG_reduced.to_indices[centre_point]]
         # Set type of node in the reduced lattice as a p-squeezed
         # state if all micronodes are p, else GKP.
-        RHG_reduced.nodes[centre_point]["state"] = reduced_state
+        reduced_graph.nodes[centre_point]["state"] = reduced_state
         # Old and permuted indices of all micronodes in macronode i.
         old_inds = [i, i + 1, i + 2, i + 3]
         old_inds.pop(star_ind - i)
@@ -117,18 +108,16 @@ def reduce_macro_and_simulate(RHG_macro, RHG_reduced, CVRHG_reduced, bs_network,
         # with 1 being the star index and the rest being planets.
         k = 1
         for ind in new_inds:
-            CVRHG.egraph.nodes[to_points[ind]]["body_index"] = k
+            reduced_graph.nodes[to_points[ind]]["body_index"] = k
             k += 1
         permuted_inds[[i, i + 1, i + 2, i + 3]] = new_inds
 
-    # Indices of stars and planets.
-    stars = permuted_inds[::4]
-    planets = np.delete(permuted_inds, np.arange(0, N, 4))
+    return permuted_inds
 
-    # Update quadrature values after CZ gate application.
-    quads = CVRHG._init_quads
-    quads = SCZ_apply(RHG_macro.adj_mat, quads)
 
+def _entangle_states(macro_graph, initial_quads, symp_bs):
+    """Entangle the states in the lattice."""
+    quads = SCZ_apply(macro_graph.adj_mat, initial_quads)
     # Permute the quadrature values to align with the permuted
     # indices in order to apply the beamsplitter network.
     quad_permutation = np.concatenate([permuted_inds, N + permuted_inds])
@@ -136,36 +125,47 @@ def reduce_macro_and_simulate(RHG_macro, RHG_reduced, CVRHG_reduced, bs_network,
     for i in range(0, N - 3, 4):
         q_inds = np.array([i, i + 1, i + 2, i + 3])
         p_inds = q_inds + N
-        updated_qs = bs_network[:4, :4] @ permuted_quads[q_inds]
-        updated_ps = bs_network[4:, 4:] @ permuted_quads[p_inds]
+        updated_qs = symp_bs[:4, :4] @ permuted_quads[q_inds]
+        updated_ps = symp_bs[4:, 4:] @ permuted_quads[p_inds]
         permuted_quads[q_inds] = updated_qs
         permuted_quads[p_inds] = updated_ps
+    return permuted_quads, quad_permutation
 
-    unpermuted_quads = permuted_quads[invert_permutation(quad_permutation)]
+
+def _measure_syndrome(noisy_macro_state, permuted_inds, entangled_quads, quad_permutation):
+    """Measure the syndrome of noisy_macro_state."""
+    unpermuted_quads = entangled_quads[invert_permutation(quad_permutation)]    
+    # Indices of stars and planets.
+    stars = permuted_inds[::4]
+    planets = np.delete(permuted_inds, np.arange(0, N, 4))
+    # Update quadrature values after CZ gate application.
     # Measure stars in p, planets in q.
-    CVRHG.measure_hom(quad="p", inds=stars, updated_quads=unpermuted_quads)
-    CVRHG.measure_hom(quad="q", inds=planets, updated_quads=unpermuted_quads)
+    noisy_macro_state.measure_hom(quad="p", inds=stars, updated_quads=unpermuted_quads)
+    noisy_macro_state.measure_hom(quad="q", inds=planets, updated_quads=unpermuted_quads)
+    
 
-    def neighbor_of_i(i, j):
-        """Return the neighbor of the ith micronode, jth macronode.
+def _neighbor_of_micro_i_macro_j(i, j, macro_graph, permuted_inds):
+    """Return the neighbor of the ith micronode, jth macronode in macro_lattice.
 
-        Micronode i is adjacent to a neighbor with a body index (1 for
-        star, 2, 3, 4 for planets). Return the vertex and the body index
-        of the neighbor to help the processing rules. If there is no
-        such neighbor, return None.
-        """
-        # Index of ith micronode in the jth macronode.
-        ith_index = permuted_inds[j + i - 1]
-        ith_vertex = to_points[ith_index]
-        # Vertex of the neighbor of the ith micronode.
-        ith_adjacency = list(CVRHG.egraph[ith_vertex])
-        if ith_adjacency:
-            ith_neighbor = list(CVRHG.egraph[ith_vertex])[0]
-            ith_body_index = CVRHG.egraph.nodes[ith_neighbor]["body_index"]
-            return ith_neighbor, ith_body_index
-        return None
+    Micronode i is adjacent to a neighbor with a body index (1 for
+    star, 2, 3, 4 for planets). Return the vertex and the body index
+    of the neighbor to help the processing rules. If there is no
+    such neighbor, return None.
+    """
+    to_points = macro_graph.to_points
+    # Index of ith micronode in the jth macronode.
+    ith_index = permuted_inds[j + i - 1]
+    ith_vertex = to_points[ith_index]
+    # Vertex of the neighbor of the ith micronode.
+    ith_adjacency = list(macro_graph[ith_vertex])
+    if ith_adjacency:
+        ith_neighbor = list(macro_graph[ith_vertex])[0]
+        ith_body_index = macro_graph[ith_neighbor]["body_index"]
+        return ith_neighbor, ith_body_index
+    return None
 
-    def m(vertex):
+
+ def _hom_outcomes(vertex, macro_graph):
         """Measurement outcomes in the macronode containing vertex.
 
         Return the values of the homodyne measurements of the macronode
@@ -180,89 +180,118 @@ def reduce_macro_and_simulate(RHG_macro, RHG_reduced, CVRHG_reduced, bs_network,
         # The central node corresponding to the neighboring
         # macronode.
         central_node = tuple(round(i) for i in vertex)
-        for micro in CVRHG.egraph.macro_to_micro[central_node]:
-            index = CVRHG.egraph.nodes[micro]["body_index"]
+        for micro in macro_graph.macro_to_micro[central_node]:
+            index = macro_graph.nodes[micro]["body_index"]
             # Populate meas with the q-homodyne outcomes for
             # the planet modes.
             if index != 1:
-                meas[index] = CVRHG.egraph.nodes[micro]["hom_val_q"]
+                meas[index] = macro_graph.nodes[micro]["hom_val_q"]
         return meas
 
-    def Z(M, neighbor_body_index):
-        """Process the homodyne outcomes for neighboring macronode i.
 
-        Macronode j is connected to a macronode whose with an array of
-        measurement outcomes M and whose micronodes have body indices
-        neighbor_body_index. Use this information to process the
-        measurement outcomes M.
-        """
-        if neighbor_body_index == 1:
-            return 0
-        if neighbor_body_index == 2:
-            return M[2] - M[4]
-        if neighbor_body_index == 3:
-            return M[3] - M[4]
-        if neighbor_body_index == 4:
-            return M[2] + M[3]
-        return None
+def _process_outcomes(hom_outcomes, neighbor_body_index):
+    """Process the homodyne outcomes for neighboring macronode i.
+    
+    Macronode j is connected to a macronode whose with an array of
+    measurement outcomes M and whose micronodes have body indices
+    neighbor_body_index. Use this information to process the
+    measurement outcomes M.
+    """
+    if neighbor_body_index == 1:
+        return 0
+    if neighbor_body_index == 2:
+        return hom_outcomes[2] - hom_outcomes[4]
+    if neighbor_body_index == 3:
+        return hom_outcomes[3] - hom_outcomes[4]
+    if neighbor_body_index == 4:
+        return hom_outcomes[2] + hom_outcomes[3]
+    return None
 
-    # sorted_homodynes = np.empty(N // 4, dtype=np.float32)
-    sorted_bits = np.empty(N // 4, dtype=np.float32)
-    reduced_indices = RHG_reduced.to_indices
-    # Processing of homodyne outcomes and calculations of phase
-    # error probabilities
+
+def _reduce_jth_macronode(j, macro_graph, permuted_inds):
+    """Reduce macronode with index j."""
+    to_points = macro_graph.to_points
+    star_index = permuted_inds[j]
+    vertex = to_points[star_index]
+
+    # Here, j corresponds to the macronode and i to to micronode.
+    # i ranges from 1 to 4, to align with manuscript.
+    verts_and_inds = [_neighbor_of_micro_i_macro_j(i, j, macro_graph, permuted_inds) for i in (1, 2, 3, 4)]
+    neighbors = [tup[0] if tup else None for tup in verts_and_inds]
+    body_indices = [tup[1] if tup else None for tup in verts_and_inds]
+
+    # Array of arrays of measurement outcomes in all the
+    # macronodes adjacent to j.
+    m_arr = np.array([_hom_outcomes(neighbors[i - 1], macro_graph) for i in (1, 2, 3, 4)])
+    # Array of processed q-homodyne outcomes from neighboring
+    # macronodes of the form [0, Z(1), Z(2), Z(3), Z(4)].
+    Z_arr = np.array([0] + [_process_outcomes(m_arr[i - 1], body_indices[i - 1]) for i in (1, 2, 3, 4)])
+    # p-homodyne outcome of the star node.
+    star_p_val = macro_graph.nodes[vertex]["hom_val_p"]
+
+    # Types of state for the four micronodes directly neighboring
+    # macronode j.
+    types = [macro_graph.nodes[neighbor]["state"] if neighbor else None for neighbor in neighbors]
+    # Phase error probability and number of p-squeezed states
+    # among the four micronodes in the vicinity of macronode j
+    p_err = 0
+    num_p = 0
+    outcome = 2 * star_p_val
+    gkp_inds = []
+    for i in (1, 2, 3, 4):
+        if types[i - 1] == "p":
+            num_p += 1
+            outcome -= Z_arr[i]
+        if types[i - 1] == "GKP":
+            if delta > 0:
+                p_err += Z_err_cond(2 * delta, Z_arr[i], use_hom_val=True)
+            gkp_inds += [i]
+    if delta > 0:
+        p_err += Z_err_cond(2 * (2 + num_p) * delta, outcome, use_hom_val=True)
+    p_err = min(p_err, 0.5)
+    p_err = max(p_err, 0)
+
+    bitp = GKP_binner([outcome])[0]
+    bitq = GKP_binner(Z_arr[gkp_inds].astype(np.float64)) if gkp_inds else 0
+
+    processed_bit_val = (bitp + np.sum(bitq)) % 2
+
+    # Update the reduced RHG lattice with the effective
+    # homodyne value and the phase error probability.
+    central_vert = tuple(round(i) for i in vertex)
+    RHG_reduced.nodes[central_vert]["bit_val"] = processed_bit_val
+    sorted_bits[reduced_indices[central_vert]] = processed_bit_val
+    RHG_reduced.nodes[central_vert]["p_phase_cond"] = p_err
+    
+
+def reduce_macronode_graph(code, macro_graph, bs_network, noise_layer=None, noise_model=None):
+    """Reduce the macronode RHG lattice to the canonical lattice.
+
+    Take the macronode lattice EGraph, RHG_macro, and generate a
+    macronode CV lattice with swap-out probaiblity swap_prob and delta
+    value delta. Then, label micronodes as planets and stars, conduct
+    homodyne measurements, process these measurements, and compute
+    conditional phase error probabilities. Generate an canonical RHG
+    lattice with effective measurement outcomes, phase error
+    probabilities, and state types ('p' or 'GKP') stored as node
+    attributes.
+    """
+    if noise_layer is None:
+        noise_layer = CVLayer
+    if noise_model is None:
+        noise_model = {
+        "noise": "grn",
+        "delta": delta,
+        "sampling_order": "two-step",
+    }
+    # Sample for the initial state
+    noisy_macro_state = _apply_initial_noise(macro_graph, noise_layer, noise_model)
+    initial_quads = noisy_macro_state._init_quads
+    # Entangle macronodes
+    permuted_inds = _permute_indices(code, macro_graph)
+    entangled_quads, quad_permutation = _entangle_states(macro_graph, initial_quads, bs_network)
+    # Measure syndrome
+    _measure_syndrome(noisy_macro_state, permuted_inds, entangled_quads, quad_permutation)
+    # Process homodyne outcomes and calculate of phase error probabilities
     for j in range(0, N - 3, 4):
-        star_index = permuted_inds[j]
-        vertex = to_points[star_index]
-
-        # Here, j corresponds to the macronode and i to to micronode.
-        # i ranges from 1 to 4, to align with manuscript.
-
-        verts_and_inds = [neighbor_of_i(i, j) for i in (1, 2, 3, 4)]
-        neighbors = [tup[0] if tup else None for tup in verts_and_inds]
-        body_indices = [tup[1] if tup else None for tup in verts_and_inds]
-
-        # Array of arrays of measurement outcomes in all the
-        # macronodes adjacent to j.
-        m_arr = np.array([m(neighbors[i - 1]) for i in (1, 2, 3, 4)])
-        # Array of processed q-homodyne outcomes from neighboring
-        # macronodes of the form [0, Z(1), Z(2), Z(3), Z(4)].
-        Z_arr = np.array([0] + [Z(m_arr[i - 1], body_indices[i - 1]) for i in (1, 2, 3, 4)])
-        # p-homodyne outcome of the star node.
-        star_p_val = CVRHG.egraph.nodes[vertex]["hom_val_p"]
-
-        # Types of state for the four micronodes directly neighboring
-        # macronode j.
-        types = [RHG_macro.nodes[neighbor]["state"] if neighbor else None for neighbor in neighbors]
-        # Phase error probability and number of p-squeezed states
-        # among the four micronodes in the vicinity of macronode j
-        p_err = 0
-        num_p = 0
-        outcome = 2 * star_p_val
-        gkp_inds = []
-        for i in (1, 2, 3, 4):
-            if types[i - 1] == "p":
-                num_p += 1
-                outcome -= Z_arr[i]
-            if types[i - 1] == "GKP":
-                if delta > 0:
-                    p_err += Z_err_cond(2 * delta, Z_arr[i], use_hom_val=True)
-                gkp_inds += [i]
-        if delta > 0:
-            p_err += Z_err_cond(2 * (2 + num_p) * delta, outcome, use_hom_val=True)
-        p_err = min(p_err, 0.5)
-        p_err = max(p_err, 0)
-
-        bitp = GKP_binner([outcome])[0]
-        bitq = GKP_binner(Z_arr[gkp_inds].astype(np.float64)) if gkp_inds else 0
-
-        processed_bit_val = (bitp + np.sum(bitq)) % 2
-
-        # Update the reduced CVRHG lattice with the effective
-        # homodyne value and the phase error probability.
-        central_vert = tuple(round(i) for i in vertex)
-        RHG_reduced.nodes[central_vert]["bit_val"] = processed_bit_val
-        sorted_bits[reduced_indices[central_vert]] = processed_bit_val
-        RHG_reduced.nodes[central_vert]["p_phase_cond"] = p_err
-    CVRHG_reduced.bits = sorted_bits
-    return
+        _reduce_jth_macronode(j, macro_graph, permuted_inds)
