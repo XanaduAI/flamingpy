88aba353f6c799eedd6b871e96ca6d7fad111549
soosub <joost.bus@resident.xanadu.ai>
2022-05-19 18:47:11 +0200

    reset to last proper commit

--- flamingpy/utils/viz.py
+++ flamingpy/utils/viz.py
@@ -58,16 +58,19 @@ plot_params = {
 @mpl.rc_context(plot_params)
 def plot_integer_part(xs, ns, alpha, show=True):
     """Plot the integer part of real numbers mod alpha."""
-    xmin, xmax = alpha * (xs[0] // alpha), alpha * (xs[-1] // alpha) + alpha
-    newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
-    newxlabels = [gkp.to_pi_string(tick) for tick in newxticks]
     fig = plt.figure()
     ax = plt.gca()
+
+    xmin, xmax = alpha * (xs[0] // alpha), alpha * (xs[-1] // alpha) + alpha
+    newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
+    ax.xaxis.set_major_formatter(gkp.to_pi_string)
+
     plt.plot(xs, ns, ".")
     plt.title("Integer Part")
     plt.xlabel("$x$")
-    plt.xticks(newxticks, newxlabels)
+    plt.xticks(newxticks)
     plt.ylabel(r"$\mathrm{int}(x)$")
+
     if show:
         plt.show()
 
@@ -77,36 +80,46 @@ def plot_integer_part(xs, ns, alpha, show=True):
 @mpl.rc_context(plot_params)
 def plot_fractional_part(xs, fs, alpha, show=True):
     """Plot the fractional part of real numbers mod alpha."""
-    plt.title("Fractional Part")
-    plt.plot(xs, fs, ".")
+    fig = plt.figure()
+    ax = plt.gca()
+
     xmin, xmax = alpha * (xs[0] // alpha), alpha * (xs[-1] // alpha) + alpha
     newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
     newyticks = np.linspace(-alpha / 2, alpha / 2, num=7)
-    newxlabels = [gkp.to_pi_string(tick) for tick in newxticks]
+    ax.xaxis.set_major_formatter(gkp.to_pi_string)
     newylabels = ["{:.3f}".format(tick) for tick in newyticks[1:-1]]
     newylabels = [gkp.to_pi_string(-alpha / 2)] + newylabels + [gkp.to_pi_string(alpha / 2)]
-    plt.xticks(newxticks, newxlabels)
+
+    plt.plot(xs, fs, ".")
+    plt.title("Fractional Part")
+    plt.xticks(newxticks)
     plt.xlabel("$x$")
     plt.yticks(newyticks, newylabels)
     plt.ylabel(r"$\mathrm{frac}(x)$")
+
     if show:
         plt.show()
+
+    return fig, ax
 
 
 @mpl.rc_context(plot_params)
 def plot_GKP_bins(outcomes, bit_values, alpha, show=True):
     """Plot binned real numbers mod alpha."""
-    xmin, xmax = alpha * (outcomes[0] // alpha), alpha * (outcomes[-1] // alpha) + alpha
-    newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
-    newxlabels = [gkp.to_pi_string(tick) for tick in newxticks]
     fig = plt.figure()
     ax = plt.gca()
+
+    xmin, xmax = alpha * (outcomes[0] // alpha), alpha * (outcomes[-1] // alpha) + alpha
+    newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
+    ax.xaxis.set_major_formatter(gkp.to_pi_string)
+
     plt.plot(outcomes, bit_values, ".")
     plt.title("Binned values")
-    plt.xticks(newxticks, newxlabels)
+    plt.xticks(newxticks)
     plt.xlabel("Outcomes")
     plt.yticks([0, 1], [0, 1])
     plt.ylabel("Bit values")
+
     if show:
         plt.show()
 
@@ -116,20 +129,24 @@ def plot_GKP_bins(outcomes, bit_values, alpha, show=True):
 @mpl.rc_context(plot_params)
 def plot_Z_err_cond(hom_val, error, alpha, use_hom_val, show=True):
     """Plot conditional phase probabilities for GKP states."""
+    fig = plt.figure()
+    ax = plt.gca()
+
     _, frac = gkp.GKP_binner(hom_val, return_fraction=True)
     val = hom_val if use_hom_val else frac
     xmin, xmax = alpha * (hom_val[0] // alpha), alpha * (hom_val[-1] // alpha) + alpha
     print(xmin, xmax, min(val), max(val))
+
     newxticks = np.linspace(xmin, xmax, int((xmax - xmin) // alpha) + 1)
-    newxlabels = [gkp.to_pi_string(tick) for tick in newxticks]
-    fig = plt.figure()
-    ax = plt.gca()
+    ax.xaxis.set_major_formatter(gkp.to_pi_string)
+
     plt.plot(val, error, ".")
+    plt.xticks(newxticks)
     plt.xlabel("Homodyne value")
     plt.ylabel("Error")
     addendum = "Full homodyne value" if use_hom_val else "Central peak"
     plt.title("Conditional phase probabilities: " + addendum)
-    plt.xticks(newxticks, newxlabels)
+
     if show:
         plt.show()
 
@@ -302,6 +319,7 @@ def draw_EGraph(
         ax.axis("off")
     plt.tight_layout(pad=5)
     plt.draw()
+    
     return fig, ax
 
 
@@ -693,3 +711,8 @@ def draw_decoding(code, ec, dec_objects=None, drawing_opts=None):
     _, ax = syndrome_plot(code, ec, drawing_opts=drawing_opts, index_dict=node_labels)
     if drawing_opts.get("show_recovery"):
         draw_recovery(ax, show_title=drawing_opts.get("show_title"), **dec_objects)
+
+
+class GKPFormatter(mpl.ticker.Formatter):
+    def __call__(self, x, pos=None):
+        return gkp.to_pi_string(x)
