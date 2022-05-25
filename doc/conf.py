c5f464d6ca201f068045f8fb242e6343eec4e465
Luis Mantilla <52287586+BestQuark@users.noreply.github.com>
2022-05-25 11:50:40 -0400

    Add UML diagrams to documentation (#41)
    * uml svgs
    
    * adds uml to doc
    
    * minor change
    
    * minor change
    
    * generates uml automatically everytime the docs are generated
    
    * changes color of uml
    
    * add uml svg files (these are generated and overwritten aanyways)
    
    * small change to add uml diagrams
    
    * reformat
    
    * i forgot to git add
    
    * scrollbar uml diagrams
    
    * changes from pylint
    
    * fix issue
    
    * fix issues
    
    * fix issues
    
    * fix issues
    
    * fix issues
    
    * change changelog.md
    
    * Revert "change changelog.md"
    
    This reverts commit bd81624fc1f32e947fa12d34920a145e72da0be2.
    
    * change changelog
    
    * fix issues
    
    * fix issues
    
    * fix issues and resize of uml diagrams
    
    * fix issues
    
    * fix issues
    
    * fix issues?
    
    * test
    
    * update conf file
    
    * setup graphviz
    
    * `graphviz_output_format = "svg"`
    
    * add blank line
    
    * very small change
    
    * Update .coveragerc
    
    * Update .coveragerc
    
    * Update _version.py
    
    * Update CHANGELOG.md
    
    * Update fp.decoders.rst
    
    * Update docs.yaml
    
    * Update CHANGELOG.md
    
    * small change
    
    * update dev_requirements.txt
    
    * fix mistake
    
    * remove pylint from dev_reqs
    
    * Update fp.rst
    
    Co-authored-by: Luis Mantilla Calderon <luismantilla@Luiss-MacBook-Pro.local>
    Co-authored-by: sduquemesa <675763+sduquemesa@users.noreply.github.com>
    Co-authored-by: Narimanium <25132802+nariman87@users.noreply.github.com>
    Co-authored-by: ilan-tz <57886357+ilan-tz@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -8,7 +8,9 @@
 """
 flamingpy configuration file for the Sphinx documentation builder.
 """
-import os, sys, re
+import os, sys, re, time
+import subprocess
+import shlex
 from unittest.mock import MagicMock
 
 
@@ -79,9 +81,10 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
-    "sphinx.ext.inheritance_diagram",
     "sphinx_gallery.gen_gallery",
+    "sphinx.ext.inheritance_diagram",
 ]
+graphviz_output_format = "svg"
 
 intersphinx_mapping = {"https://flamingpy.readthedocs.io/en/latest/": None}
 
@@ -152,6 +155,32 @@ show_authors = True
 # pixels large.
 html_favicon = "_static/favicon.ico"
 
+# Creates UML diagrams (svg). These are later used in source/fp.rst .
+comd = "pyreverse -o svg -p flamingpy ../flamingpy -d _static --colorized --max-color-depth 1 -k"
+subprocess.call(
+    shlex.split(comd),
+    shell=False,
+)
+time.sleep(0.5)
+
+# Changes color of uml diagrams to match the common theme
+with open("_static/packages_flamingpy.svg", "r", encoding="utf-8") as file:
+    filedata = file.read()
+filedata = filedata.replace("aliceblue", "#bde0ff")
+filedata = filedata.replace("Times,serif", "Helvetica,sans-serif")
+filedata = filedata.replace("green", "black")
+filedata = filedata.replace('font-size="14.00"', 'font-size="12.00"')
+with open("_static/packages_flamingpy.svg", "w", encoding="utf-8") as file:
+    file.write(filedata)
+
+with open("_static/classes_flamingpy.svg", "r", encoding="utf-8") as file:
+    filedata = file.read()
+filedata = filedata.replace("aliceblue", "#bde0ff")
+filedata = filedata.replace("Times,serif", "Helvetica,sans-serif")
+filedata = filedata.replace("green", "black")
+filedata = filedata.replace('font-size="14.00"', 'font-size="12.00"')
+with open("_static/classes_flamingpy.svg", "w", encoding="utf-8") as file:
+    file.write(filedata)
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
