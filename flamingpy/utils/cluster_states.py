22506f22ee0bdd0092c3ea734e609a2db83a78c8
Luis Mantilla <luismantilla99@outlook.com>
2022-05-30 11:01:32 -0400

    formmating

--- flamingpy/utils/cluster_states.py
+++ flamingpy/utils/cluster_states.py
@@ -16,69 +16,75 @@
 import numpy as np
 from flamingpy.codes.graphs import EGraph
 
+
 def star_graph_state(n):
     """
-    Returns the EGraph of a star graph state with n nodes. 
+    Returns the EGraph of a star graph state with n nodes.
     """
-    assert isinstance(n,int), f"Input n should be an integer. Current type is {type(n)}"
-    assert n>=2, f"Input n should be 2 or larger. Current value is {n}"
+    assert isinstance(n, int), f"Input n should be an integer. Current type is {type(n)}"
+    assert n >= 2, f"Input n should be 2 or larger. Current value is {n}"
     star_graph_state = EGraph()
-    for i in range(n-1):
-        degs=2*np.pi*i/(n-1)
+    for i in range(n - 1):
+        degs = 2 * np.pi * i / (n - 1)
         x, y = np.cos(degs), np.sin(degs)
         edge = [(0, 0, 0), (x, y, 0)]
-        star_graph_state.add_edge(*edge, color = "MidnightBlue")
+        star_graph_state.add_edge(*edge, color="MidnightBlue")
     return star_graph_state
-    
+
+
 def ghz_state(n):
     """
     Returns the EGraph of a GHZ state with n nodes.
     """
-    assert isinstance(n,int), f"Input n should be an integer. Current type is {type(n)}"
-    assert n>=3, f"Input n should be 3 or larger. Current value is {n}"
+    assert isinstance(n, int), f"Input n should be an integer. Current type is {type(n)}"
+    assert n >= 3, f"Input n should be 3 or larger. Current value is {n}"
     ghz_state = EGraph()
     for i in range(n):
-        degs=2*np.pi*i/n
+        degs = 2 * np.pi * i / n
         x, y = np.cos(degs), np.sin(degs)
-        for j in range(1,n):
-            degs_adj = 2*np.pi*j/n + degs
+        for j in range(1, n):
+            degs_adj = 2 * np.pi * j / n + degs
             x_adj, y_adj = np.cos(degs_adj), np.sin(degs_adj)
             edge = [(x, y, 0), (x_adj, y_adj, 0)]
-            ghz_state.add_edge(*edge, color = "MidnightBlue")
+            ghz_state.add_edge(*edge, color="MidnightBlue")
     return ghz_state
+
 
 def linear_state(n):
     """
     Returns the EGraph of a linear cluster state with n nodes.
     """
-    assert isinstance(n,int), f"Input n should be an integer. Current type is {type(n)}"
-    assert n>=2, f"Input n should be 2 or larger. Current value is {n}"
+    assert isinstance(n, int), f"Input n should be an integer. Current type is {type(n)}"
+    assert n >= 2, f"Input n should be 2 or larger. Current value is {n}"
     linear_state = EGraph()
-    for i in range(n-1):
-        edge = [(i, 0, 0), (i+1, 0, 0)]
-        linear_state.add_edge(*edge, color = "MidnightBlue")
+    for i in range(n - 1):
+        edge = [(i, 0, 0), (i + 1, 0, 0)]
+        linear_state.add_edge(*edge, color="MidnightBlue")
     return linear_state
+
 
 def ring_state(n):
     """
-    Returns the EGraph of a ring graph state with n nodes. 
+    Returns the EGraph of a ring graph state with n nodes.
     """
-    assert isinstance(n,int), f"Input n should be an integer. Current type is {type(n)}"
-    assert n>=3, f"Input n should be 3 or larger. Current value is {n}"
+    assert isinstance(n, int), f"Input n should be an integer. Current type is {type(n)}"
+    assert n >= 3, f"Input n should be 3 or larger. Current value is {n}"
     ring_graph_state = EGraph()
     for i in range(n):
-        degs=2*np.pi*i/(n)
-        degs_next=2*np.pi*(i+1)/(n)
-        x, y = np.cos(degs), np.sin(degs) 
-        x_next, y_next = np.cos(degs_next), np.sin(degs_next) 
+        degs = 2 * np.pi * i / (n)
+        degs_next = 2 * np.pi * (i + 1) / (n)
+        x, y = np.cos(degs), np.sin(degs)
+        x_next, y_next = np.cos(degs_next), np.sin(degs_next)
         edge = [(x, y, 0), (x_next, y_next, 0)]
-        ring_graph_state.add_edge(*edge, color = "MidnightBlue")
-    return ring_graph_state 
+        ring_graph_state.add_edge(*edge, color="MidnightBlue")
+    return ring_graph_state
+
 
 def bell_state(n=2):
     """
     Returns the EGraph of a two-qubit bell state.
     """
-    assert n==2, "Bell states are defined for two qubits. For larger n, you might want to consider GHZ states"
+    assert (
+        n == 2
+    ), "Bell states are defined for two qubits. For larger n, you might want to consider GHZ states"
     return linear_state(n)
-
