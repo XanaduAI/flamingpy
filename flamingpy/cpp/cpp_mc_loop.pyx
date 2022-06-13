51f95061b821b2c5f660cf8b6fd48387cc12818c
ilan-tz <57886357+ilan-tz@users.noreply.github.com>
2022-06-13 19:41:47 -0400

    Update cpp_mc_loop.pyx
    Fix cpp loop to align with changes in branch

--- flamingpy/cpp/cpp_mc_loop.pyx
+++ flamingpy/cpp/cpp_mc_loop.pyx
@@ -13,24 +13,14 @@
 # limitations under the License.
 """Cython-based Monte Carlo simulations for estimating FT thresholds."""
 from flamingpy.decoders.decoder import correct
-from flamingpy.noise import CVLayer, CVMacroLayer
 
-cpdef int cpp_mc_loop(object code, int trials, dict decoder, dict weight_options, object macro_graph, double p_swap, double delta, dict cv_noise):
+cpdef int cpp_mc_loop(int trials, object code_instance, object noise_instance, dict decoder, dict weight_options):
     """Exclusively run loop section of Monte Carlo simulations of error-correction on code=code."""
-    noise_model = {"noise": "grn", "delta": delta}
     cdef int successes = 0
     cdef int result, errors, ii
     for ii in range(trials):
-        if macro_graph:
-            CV_macro = CVMacroLayer(macro_graph, p_swap=p_swap, reduced_graph=code.graph)
-            CV_macro.reduce(noise_model)
-        else:
-            # Apply noise
-            CVRHG = CVLayer(code, p_swap=p_swap)
-            CVRHG.apply_noise(cv_noise)
-            # Measure syndrome
-            CVRHG.measure_hom("p", code.syndrome_inds)
-        result = correct(code=code, decoder=decoder, weight_options=weight_options)
+        noise_instance.apply_noise()
+        result = correct(code=code_instance, decoder=decoder, weight_options=weight_options)
         successes += result
     errors = trials - successes
     return errors
