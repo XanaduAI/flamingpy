43c93a82192dcffc11562ba7d57919fb5014bb18
nariman87 <25132802+nariman87@users.noreply.github.com>
2022-09-05 20:08:50 +1000

    Updating viz module init file

--- flamingpy/viz/__init__.py
+++ flamingpy/viz/__init__.py
@@ -12,5 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The visualization module."""
-#from .iid import IidNoise
-#from .cv import CVLayer, CVMacroLayer
